6e27bc2e62cf2b6f2e48096df852be295d2b69c7
Chukwurado98 <chukwurado.umeaka98@gmail.com>
2019-11-05 14:58:32 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,89 @@
+from tkinter import *
+from calculate import calculate
+
+def show(val):
+    if resultField.get('0.0', END) == '0\n':
+        resultField.delete("0.0", END)
+        resultField.insert(INSERT, val)
+    else:
+        ops = {'-', '+', '*', '/'}
+        prev = resultField.get("0.0", 'end-1c')
+        resultField.delete("0.0", END)
+        if prev[len(prev) - 1] not in ops and val == ' ( ':
+            prev += ' *'
+        prev += val
+        resultField.insert(INSERT, prev)
+
+def clear():
+    resultField.delete('0.0', END)
+    resultField.insert(INSERT, '0')
+
+def calc():
+    exp = resultField.get("0.0", 'end-1c')
+    res = calculate(exp)
+    resultField.delete('0.0', END)
+    resultField.insert(INSERT, str(res))
+
+window = Tk()
+
+window.wm_title("Calculator")
+
+resultField = Text(window, height=1, width=15)
+resultField.insert(INSERT, "0")
+resultField.grid(row=1, column=1, columnspan=3)
+
+b9 = Button(window, text="9", width=5, command=lambda : show("9"))
+b9.grid(row=2, column=3)
+
+b8 = Button(window, text="8", width=5, command=lambda : show("8"))
+b8.grid(row=2, column=2)
+
+b7 = Button(window, text="7", width=5, command=lambda : show("7"))
+b7.grid(row=2, column=1)
+
+b6 = Button(window, text="6", width=5, command=lambda : show("6"))
+b6.grid(row=3, column=3)
+
+b5 = Button(window, text="5", width=5, command=lambda : show("5"))
+b5.grid(row=3, column=2)
+
+b4 = Button(window, text="4", width=5, command=lambda : show("4"))
+b4.grid(row=3, column=1)
+
+b3 = Button(window, text="3", width=5, command=lambda : show("3"))
+b3.grid(row=4, column=3)
+
+b2 = Button(window, text="2", width=5, command=lambda : show("2"))
+b2.grid(row=4, column=2)
+
+b1 = Button(window, text="1", width=5, command=lambda : show("1"))
+b1.grid(row=4, column=1)
+
+bdiv = Button(window, text="/", width=5, command=lambda : show(" / "))
+bdiv.grid(row=2, column=4)
+
+dmul = Button(window, text="x", width=5, command=lambda : show(" * "))
+dmul.grid(row=3, column=4)
+
+bminus = Button(window, text="-", width=5, command=lambda : show(" - "))
+bminus.grid(row=4, column=4)
+
+badd = Button(window, text="+", width=5, command=lambda : show(" + "))
+badd.grid(row=5, column=4)
+
+bOpenParen = Button(window, text="(", width=5, command=lambda : show(" ( "))
+bOpenParen.grid(row=5, column=2)
+
+bCloseParen = Button(window, text=")", width=5, command=lambda : show(" ) "))
+bCloseParen.grid(row=5, column=3)
+
+b0 = Button(window, text="0", width=5, command=lambda : show("9"))
+b0.grid(row=5, column=1)
+
+bClear = Button(window, text="C", width=10, command=lambda : clear())
+bClear.grid(row=6, column=1, columnspan=2)
+
+bCalc = Button(window, text="=", width=10, command=lambda : calc())
+bCalc.grid(row=6, column=3, columnspan=2)
+
+window.mainloop()
---
+++ calculate.py
@@ -0,0 +1,71 @@
+import re
+
+def calculate(exp):
+    exp_arr = exp.strip().split(' ')
+    post_exp = infix_to_postfix(exp_arr)
+    result = calc_postfix(post_exp)
+    print(result)
+    return result
+    
+def infix_to_postfix(arr):
+    operators = {
+            '*': 3, 
+            '/': 3,
+            '+': 2,
+            '-': 2,
+            '(': 1,
+            ')': 1
+        }
+    postfix = []
+    oper_stack = []
+    for o in arr:
+        if not operators.get(o):
+            postfix.append(float(o))
+        else:
+            if len(oper_stack) == 0:
+                oper_stack.append(o)
+            else:
+                if o == "(":
+                    oper_stack.append(o)
+                elif o == ")":
+                    while oper_stack[len(oper_stack)-1] != '(':
+                        postfix.append(oper_stack.pop())
+                    oper_stack.pop()
+                else:
+                    top = oper_stack[len(oper_stack)-1]
+                    while len(oper_stack) > 0 and operators[o] <= operators[top]:
+                        postfix.append(oper_stack.pop())
+                        if len(oper_stack) > 0:
+                            top = oper_stack[len(oper_stack)-1]
+                    oper_stack.append(o)
+    while len(oper_stack) > 0:
+        postfix.append(oper_stack.pop())
+    return postfix
+
+def calc_postfix(arr):
+    calc = []
+    for o in arr:
+        if o == '*':
+            a = calc.pop()
+            b = calc.pop()
+            calc.append(b*a)
+        elif o == '/':
+            a = calc.pop()
+            b = calc.pop()
+            calc.append(b/a)
+        elif o == '+':
+            a = calc.pop()
+            b = calc.pop()
+            calc.append(b+a)
+        elif o == '-':
+            a = calc.pop()
+            b = calc.pop()
+            calc.append(b-a)
+        else:
+            calc.append(o)
+    return calc.pop()
+
+
+# calculate("( 42.3 / 3 ) - 34 ")
+# calculate("1 + 2 * 4.2")
+# # 6 -7 * 7 + 5 -> 677*-5+
